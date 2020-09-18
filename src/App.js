4637198d31c991a762757457b3d9b02c5ac0def2
tanner.dailey <daileytanner@gmail.com>
2020-09-18 16:53:42 -0600

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Home</h2>
       </header>
+      <section className='card'>
+        <div className='info'>
+
+        </div>
+        <div className='page'>
+          <p>1/25</p>
+        </div>
+      </section>
+      <div className='buttons'>
+        <a href=''>Prev</a>
+        <div className='mid-buttons'>
+          <button>Edit</button>
+          <button>Delete</button>
+          <button>New</button>
+        </div>
+        <a href=''>Next</a>
+      </div>
     </div>
   );
 }
